f262088517ca2f44220fe9acab49a922c3f5177e
Bipin <biping579@gmail.com>
2022-11-12 00:31:26 +0530

    login updated

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -658,96 +658,97 @@ let navbar = () => {
 
         <!-- **********************Whislist section cart login******************************** -->
         <div class="header-right">
-          <ul class="main-menu">
-            <li class="menu-item">
-              <img
-                src="https://cdn-icons-png.flaticon.com/128/206/206626.png"
-                alt="flag"
-              />
-            </li>
-            <li class="menu-item">
-              <img
-                src="https://cdn-icons-png.flaticon.com/128/2529/2529521.png"
-                alt=""
-              />
-            </li>
-            <li class="menu-item">
-              <img
-                src="https://cdn-icons-png.flaticon.com/128/6205/6205825.png"
-                alt=""
-              />
-            </li>
-            <li class="menu-item" data-modal-target="#modal">
-              <img
-                src="https://cdn-icons-png.flaticon.com/128/456/456212.png"
-                alt=""
-              />
-            </li>
-            <li class="menu-item">
-              <img
-                src="https://cdn-icons-png.flaticon.com/128/482/482631.png"
-                alt=""
-              /><span style="font-size: large"> Search</span>
-            </li>
-          </ul>
-        </div>
+        <ul class="main-menu">
+          <li class="menu-item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/206/206626.png"
+              alt="flag"
+            />
+          </li>
+          <li class="menu-item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/2529/2529521.png"
+              alt=""
+            />
+          </li>
+          <li class="menu-item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/6205/6205825.png"
+              alt=""
+            />
+          </li>
+          <li class="menu-item" data-modal-target="#modal">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/456/456212.png"
+              alt=""
+            />
+          </li>
+          <li class="menu-item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/482/482631.png"
+              alt=""
+            /><span style="font-size: large"> Search</span>
+          </li>
+        </ul>
       </div>
+    </div>
 
-      <!-- <button data-modal-target="#modal">Open Modal</button> -->
-      <div class="modal" id="modal">
-        <div class="modal-header">
-            <div class="title">SIGNUP HERE</div>
-            <button data-close-button class="close-button">&times;</button>
-        </div>
-          <div class="modal-body">
-                <!-- left side modal start -->
-                <div class="modal-left">
-                    <div class="logo">
-                        <img
-                        src="https://cdn.modesens.com/static/img/20221102whitelogo-assistant-en.svg"
-                        alt=""
-                        width="200px"
-                        background-color="white"
-                        />
-                    </div>
-                    <div class="icons-left-modal">
-                        <ul>
-                            <li><span class="las la-shopping-bag"></span><h4>COMPARE ACROSS 500+ STORES</h4></li>
-                            <li><span class="lar la-bell"></span><h4>GET ALERTS ON YOUR ITEM</h4></li>
-                            <li><span class="las la-file-download"></span><h4>SAVE YOUR SEARCHES</h4></li>
-                            <li><span class="lar la-heart"></span><h4>MANAGE YOUR SHOPPING LIST</h4></li>
-                            <li><span class="las la-trophy"></span><h4>EARN POINTS</h4></li>
-                            
-                        </ul>
-                    </div>
-                </div>
+    <!-- <button data-modal-target="#modal">Open Modal</button> -->
+    <div class="modal" id="modal">
+      <div class="modal-header">
+          <div class="mtitle">SIGNUP HERE</div>
+          <button data-close-button class="close-button">&times;</button>
+      </div>
+        <div class="modal-body">
+              <!-- left side modal start -->
+              <div class="modal-left">
+                  <div class="logo">
+                      <img
+                      src="https://cdn.modesens.com/static/img/20221102whitelogo-assistant-en.svg"
+                      alt=""
+                      width="200px"
+                      background-color="white"
+                      />
+                  </div>
+                  <div class="icons-left-modal">
+                      <ul>
+                          <li><span class="las la-shopping-bag"></span><h4>COMPARE ACROSS 500+ STORES</h4></li>
+                          <li><span class="lar la-bell"></span><h4>GET ALERTS ON YOUR ITEM</h4></li>
+                          <li><span class="las la-file-download"></span><h4>SAVE YOUR SEARCHES</h4></li>
+                          <li><span class="lar la-heart"></span><h4>MANAGE YOUR SHOPPING LIST</h4></li>
+                          <li><span class="las la-trophy"></span><h4>EARN POINTS</h4></li>
+                          
+                      </ul>
+                  </div>
+              </div>
 
-                <div class="modal-right">
-                    <div class="form_div">
-                        <h3 style="text-align: center; margin-bottom: 15px;">Create an Acoount</h3>
-                        <form action="" id="login_form">
-                            <input type="email" placeholder="Email" id="email">
-                            <input type="text" placeholder="Password" id="password">
-                            <p><input type="checkbox"> <span>Subscribe to personalized sale updates and offers</span></p>
-                        </form>
-                        
-                        <button class="signup">SIGN UP</button>
-                        <p style="margin: 10px 0;">OR</p>
-                    </div>
+              <div class="modal-right">
+                  <div class="form_div">
+                      <h3 class="right_title" style="text-align: center; margin-bottom: 15px;">Create an Acoount</h3>
+                      <form action="" id="login_form">
+                          <input type="email" placeholder="Email" id="email">
+                          <input type="password" placeholder="Password" id="password">
+                          <p class="checkbox"><input type="checkbox"> <span>Subscribe to personalized sale updates and offers</span></p>
+                      </form>
+                      
+                      <button id="submit" class="signup ">SIGN UP</button>
+                      <button id="login" class="signup ">SIGN IN</button>
+                      <p style="margin: 10px 0;">OR</p>
+                  </div>
 
-                    <div class="form_icons">
-                        <img src="https://cdn-icons-png.flaticon.com/128/300/300221.png" alt="">
-                        <img src="https://cdn-icons-png.flaticon.com/128/3128/3128304.png" alt="">
-                        <img src="https://cdn-icons-png.flaticon.com/128/731/731985.png" alt="">
-                        <img src="https://cdn-icons-png.flaticon.com/128/889/889153.png" alt="">
-                    </div>
+                  <div class="form_icons">
+                      <img src="https://cdn-icons-png.flaticon.com/128/300/300221.png" alt="">
+                      <img src="https://cdn-icons-png.flaticon.com/128/3128/3128304.png" alt="">
+                      <img src="https://cdn-icons-png.flaticon.com/128/731/731985.png" alt="">
+                      <img src="https://cdn-icons-png.flaticon.com/128/889/889153.png" alt="">
+                  </div>
 
-                    <p class="already">Alredy have an account?Please SignIn</p>
-                </div>
-            </div>
-        </div>
-      <div id="moverlay"></div>
-    </header>
+                  <p onclick="Toggle()" id="already">Alredy have an account?Please SignIn</p>
+              </div>
+          </div>
+      </div>
+    <div id="moverlay"></div>
+  </header>
     `
 }
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -49,7 +49,139 @@ function closeModal(modal) {
   overlay.classList.remove('active')
 }
 
-//popup js end
+
+
+let signupBtn = document.querySelector("#submit");
+
+signupBtn.addEventListener("click", () => {
+  signup();
+});
+
+const api = `https://modesens-data3.onrender.com/api/user`;
+
+const signup =async () => {
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  
+  let data={
+    email,
+    password,
+    cart: []
+}
+let exist=await fetch(api)
+exist=await exist.json();
+
+if (exist.find(e => e.email===email)) {
+    /* same result as above, but a different function return type */
+    alert(`${email} already exist.Please Login`);
+    // window.location.href="./login.html"
+    return;
+  }else{
+    await fetch(api,{
+        method:"POST",
+        body:JSON.stringify(data),
+        headers:{
+            "content-Type":"application/json",
+        }
+    });
+    let form = document.getElementById("login_form");
+      form.reset();
+    alert(`Welcome ${name} to Modesens`)
+    window.location.href="/divine-society-1574/index.html"
+
+    
+  }
+
+
+}
+
+let loginBtn = document.getElementById("login");
+let SingnInBtn = document.getElementById("submit");
+loginBtn.style.display = "none";
+let already = document.querySelector("#already");
+already.onclick = () => {
+  Toggle();
+}
+
+let toggler = false;
+function Toggle() {
+  let title = document.querySelector(".mtitle");
+  let form_title = document.querySelector(".right_title");
+  let p = document.querySelector(".checkbox");
+  let addClass = document.querySelector("#submit");
+  let already = document.querySelector("#already");
+  
+  if (toggler == false) {
+    title.innerText = "LOGIN HERE";
+    form_title.innerText = "Sign In here to Continue";
+    p.innerHTML = "Forgot Password"
+    p.style.marginBottom = "15px";
+    already.innerText = "Create an Account";
+    addClass.innerText = "Sign In";
+    addClass.style.width = "70%"
+    addClass.style.height = "30px";
+    // addClass.classList.add("login");
+    // addClass.classList.remove("signup");
+    loginBtn.style.display = "block";
+    SingnInBtn.style.display = "none";
+    toggler = true;
+  }
+  else {
+    title.innerText = "SIGN UP HERE";
+    form_title.innerText = "Create an Acoount";
+    p.innerText = "Subscribe to personalized sale updates and offers";
+    already.innerText = "Alredy have an account?Please SignIn";
+    addClass.innerText = "Sign Up";
+    addClass.style.width = "70%"
+    addClass.style.height = "30px";
+    
+    loginBtn.style.display = "none";
+    SingnInBtn.style.display = "block";
+
+    toggler = false;
+  }
+  
+  
+}
+
+
+let signinBtn = document.querySelector("#login");
+
+signinBtn.addEventListener("click", () => {
+  signin();
+});
+
+
+
+let signin = async() => {
+  let mail = document.getElementById('email').value;
+  let pass = document.getElementById('password').value;
+
+  let data;
+    try {
+        data=await fetch(api)
+        data=await data.json();
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+
+    if(data.find(e=>e.email==mail && e.password==pass)){
+        
+      let form = document.getElementById("login_form");
+      form.reset();
+      alert('Login Successfull')
+      window.location.href="/divine-society-1574/index.html"
+    }else{
+        alert("Wrong Credentials")
+    }
+  
+}
+
+
+
+
+
 
 
 
